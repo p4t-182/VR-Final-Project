f463f4dd979c71ea617ebbb9b525d703237776af
unknown <ctnnbeko@gmail.com>
2024-11-14 11:06:14 -0800

    Added gopher script

--- Assets/GopherTest.cs
+++ Assets/GopherTest.cs
@@ -4,11 +4,39 @@ using UnityEngine;
 
 public class GopherTest : MonoBehaviour
 {
-    private SpringJoint puck;
+
+    [SerializeField]
+    private List<GameObject> pucks;
+    private int listLen, randIdx, prevIdx;
+    private float prevTime, currTime;
+
     // Update is called once per frame
     void Start()
     {
-        puck = GetComponent<SpringJoint>();
-        puck.spring = 500;
+        prevTime = Time.time;
+        if(pucks != null)
+        {
+            listLen = pucks.Count;
+            for (int i = 0; i < listLen; i++)
+            {
+                pucks[i].GetComponent<SpringJoint>().spring = 0;
+            }
+            randIdx = Random.Range(0, listLen);
+        }
+        
+    }
+
+    void Update()
+    {
+        currTime = Time.time;
+        prevIdx = randIdx;
+        pucks[randIdx].GetComponent<SpringJoint>().spring = 100;
+        if((currTime - prevTime) > 2.0f)
+        {
+            prevTime = currTime;
+            randIdx = Random.Range(0, listLen);
+            pucks[prevIdx].GetComponent<SpringJoint>().spring = 0;
+        }
+    
     }
 }
