587a567a242e6f678d5bb0cdda98ded78e2b8b7a
JosephPCS <89505711+JosephPCS@users.noreply.github.com>
2024-11-19 09:43:29 -0700

    added game script

--- Assets/whack.cs
+++ Assets/whack.cs
@@ -9,8 +9,7 @@ public class whack : MonoBehaviour
     // Start is called before the first frame update
 
 
-    // so for this when the mallet successfully hits the gopher, the player score goes up
-    // where is score being stored??
+    
 
     void OnCollisionEnter(Collision __){
         
