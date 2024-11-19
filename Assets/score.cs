61724df730d563cbaca50a83d8dc7b68cad65e32
JosephPCS <89505711+JosephPCS@users.noreply.github.com>
2024-11-19 10:13:00 -0700

    updated score and game scripts

--- Assets/score.cs
+++ Assets/score.cs
@@ -8,21 +8,12 @@ public static class Score
 
     public static int totalscore = 0;
 
-    public static TMPro.TMP_Text HighScore;
-    public static TMPro.TMP_Text final;
+   
 
     public static void AddScore(int v){
         Score.score += v;
     }
 
 
-    public static void FinalScore(int curr){
-        if(curr > Score.totalscore){
-            Score.totalscore = curr;
-            
-            HighScore.text =  "NEW HIGH SCORE" + curr.ToString();
-        }else{
-            final.text = "Score: " + curr.ToString();
-        }
-    }
+   
 }
