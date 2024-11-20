fd4799dae062d3f08f84ea0cb1872c71b4f3fad2
unknown <ctnnbeko@gmail.com>
2024-11-20 14:39:55 -0800

    Fixed game script and hammer

--- Assets/game.cs
+++ Assets/game.cs
@@ -2,55 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
+
+
 public class game : MonoBehaviour
 {
 
-//    /*This is the code that controls the game when it starts
-//    *What is needed for this code
-//    *- A timer that decreases to 0 (done)
-//    * when time = 0 stop the game and check for HiScore
-//    *some way to start the game
-//    *-Music goes here potentially?
-//    */
-    
-//    // Start is called before the first frame update
-   private float timer = 60f;
-   private float countdown;
-   public static TMPro.TMP_Text displayTime;
+    //    /*This is the code that controls the game when it starts
+    //    *What is needed for this code
+    //    *- A timer that decreases to 0 (done)
+    //    * when time = 0 stop the game and check for HiScore
+    //    *some way to start the game
+    //    *-Music goes here potentially?
+    //    */
 
-   public static TMPro.TMP_Text HighScore;
-   public static TMPro.TMP_Text final;
+    //    // Start is called before the first frame update
+    public TMPro.TMP_Text displayTime;
 
-   void Start()
-   {
-       timer = countdown;
-   }
+    public TMPro.TMP_Text HighScore;
+    public TMPro.TMP_Text final;
 
-    public static void FinalScore(int curr){
-       if(curr > Score.totalscore){
-           Score.totalscore = curr;
-            
-           HighScore.text =  "NEW HIGH SCORE" + curr.ToString();
-       }else{
-           final.text = "Score: " + curr.ToString();
-       }
-   }
+    private float timer = 60f;
+    private float countdown;
 
-   // Update is called once per frame
-   void Update()
-   {
-       if(countdown <= 0){
-           // get score and run it through the hiscore check
-           countdown = 0;
-           FinalScore(Score.score);
-       }else{
-           countdown -= Time.deltaTime;
-           updatetimeDisplay();
-       }
 
-   }
 
-   void updatetimeDisplay(){
-       displayTime.text = "Time: " + Mathf.CeilToInt(countdown).ToString();
-   }
+    void Start()
+    {
+        timer = countdown;
+
+    }
+
+    public void FinalScore(int curr)
+    {
+        if (curr > Score.totalscore)
+        {
+            Score.totalscore = curr;
+
+            HighScore.text = "NEW HIGH SCORE" + curr.ToString();
+        }
+        else
+        {
+            final.text = "Score: " + curr.ToString();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countdown <= 0)
+        {
+            // get score and run it through the hiscore check
+            countdown = 0;
+            FinalScore(Score.score);
+        }
+        else
+        {
+            countdown -= Time.deltaTime;
+            updatetimeDisplay();
+        }
+
+    }
+
+    void updatetimeDisplay()
+    {
+        displayTime.text = "Time: " + Mathf.CeilToInt(countdown).ToString();
+    }
 }
