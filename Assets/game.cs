18bde3b95b1fed5f784e860ad94fb4b82de472dc
JosephPCS <89505711+JosephPCS@users.noreply.github.com>
2024-11-20 14:05:17 -0700

    updated scene and fixed game.cs

--- Assets/game.cs
+++ Assets/game.cs
@@ -1,9 +1,9 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
-//public class game : MonoBehaviour
-//{
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class game : MonoBehaviour
+{
 
 //    /*This is the code that controls the game when it starts
 //    *What is needed for this code
@@ -14,43 +14,43 @@
 //    */
     
 //    // Start is called before the first frame update
-//    private float timer = 60f;
-//    private float countdown;
-//    public static TMPro.TMP_Text displayTime;
+   private float timer = 60f;
+   private float countdown;
+   public static TMPro.TMP_Text displayTime;
 
-//    public static TMPro.TMP_Text HighScore;
-//    public static TMPro.TMP_Text final;
+   public static TMPro.TMP_Text HighScore;
+   public static TMPro.TMP_Text final;
 
-//    void Start()
-//    {
-//        timer = countdown;
-//    }
+   void Start()
+   {
+       timer = countdown;
+   }
 
-//     public static void FinalScore(int curr){
-//        if(curr > Score.totalscore){
-//            Score.totalscore = curr;
+    public static void FinalScore(int curr){
+       if(curr > Score.totalscore){
+           Score.totalscore = curr;
             
-//            HighScore.text =  "NEW HIGH SCORE" + curr.ToString();
-//        }else{
-//            final.text = "Score: " + curr.ToString();
-//        }
-//    }
+           HighScore.text =  "NEW HIGH SCORE" + curr.ToString();
+       }else{
+           final.text = "Score: " + curr.ToString();
+       }
+   }
 
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        if(countdown <= 0){
-//            // get score and run it through the hiscore check
-//            countdown = 0;
-//            Score.FinalScore(Score.score);
-//        }else{
-//            countdown -= Time.deltaTime;
-//            updatetimeDisplay();
-//        }
+   // Update is called once per frame
+   void Update()
+   {
+       if(countdown <= 0){
+           // get score and run it through the hiscore check
+           countdown = 0;
+           FinalScore(Score.score);
+       }else{
+           countdown -= Time.deltaTime;
+           updatetimeDisplay();
+       }
 
-//    }
+   }
 
-//    void updatetimeDisplay(){
-//        displayTime.text = "Time: " + Mathf.CeilToInt(countdown).ToString();
-//    }
-//}
+   void updatetimeDisplay(){
+       displayTime.text = "Time: " + Mathf.CeilToInt(countdown).ToString();
+   }
+}
