8bfaa9049e03e9a4d6d04cd9eebc8b678a9cd719
JosephPCS <89505711+JosephPCS@users.noreply.github.com>
2024-12-05 10:41:57 -0700

    added lookAt script

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+
+    public GameObject player;
+    
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform);
+    }
+}
